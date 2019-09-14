9ce18aaf7c9abb809062a9d4e808686bbba8dbc5
Nikita Savinov <nikita.savinov@team.wrike.com>
2019-09-14 08:36:41 +0300

    init

---
+++ src/main/java/com/zdravstvuyderevo/hackathon/controller/CommonPage.java
@@ -0,0 +1,16 @@
+package com.zdravstvuyderevo.hackathon.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 2019-09-14 : 08:06
+ *
+ * @author Nikita Savinov
+ */
+
+@Controller
+public class CommonPage {
+
+
+
+}
